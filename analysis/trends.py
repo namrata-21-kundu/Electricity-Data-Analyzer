fa3dcf4a8d1ec62e4c70c9d88d64e66f4a6addcf
Namrata Kundu <namratakundu22work@gmail.com>
2026-07-20 03:25:37 +0530

    modify trends: add calculate summary

--- analysis/trends.py
+++ analysis/trends.py
@@ -1,24 +1,29 @@
 import pandas as pd
 from db.connection import connect_db
 
+
 def get_data():
     connection = connect_db()
 
     query = """
-            SELECT *
-            FROM readings
-            """
-    
+    SELECT *
+    FROM readings
+    """
+
     df = pd.read_sql(query, connection)
 
     connection.close()
-   
+
+    return df
+
+
+def calculate_summary(df):
     total_usage = df["usage_kwh"].sum()
     average_usage = df["usage_kwh"].mean()
     maximum_usage = df["usage_kwh"].max()
     minimum_usage = df["usage_kwh"].min()
 
-    print(f"Total Usage: {total_usage:.2f} kWh")
-    print(f"Average Usage: {average_usage:.2f} kWh")
-    print(f"Maximum Usage: {maximum_usage:.2f} kWh")
-    print(f"Minimum Usage: {minimum_usage:.2f} kWh")
+    print(f"Total Usage   : {total_usage:.2f} kWh")
+    print(f"Average Usage : {average_usage:.2f} kWh")
+    print(f"Maximum Usage : {maximum_usage:.2f} kWh")
+    print(f"Minimum Usage : {minimum_usage:.2f} kWh")
