e8e4425bc77e9ff903ccb08481bcb04958a54147
Namrata Kundu <namratakundu22work@gmail.com>
2026-07-22 01:23:20 +0530

    modify: repharse prompt

---
+++ agent/test_explain.py
@@ -0,0 +1,13 @@
+from explain import explain
+
+result = {
+    "total_units": 345,
+    "estimated_cost": 2760
+}
+
+answer = explain(
+    "Why was my electricity bill high?",
+    result
+)
+
+print(answer)
